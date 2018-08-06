60879c48ba2bd626049e5e685a6fd96bd055b549
wgebis <wgebis@gmail.com>
2018-08-06 13:25:33 +0200

    Add elk support

--- run_remind_job.py
+++ run_remind_job.py
@@ -2,11 +2,9 @@ import logging
 from flask import Flask
 from nisse.services.reminder_job import remind
 from nisse.utils.configs import load_config
-from nisse.utils.logging import init_logging
 
 application = Flask(__name__, instance_relative_config=True)
 
 load_config(application)
-init_logging(application)
 
 remind(application.logger, application.config)
