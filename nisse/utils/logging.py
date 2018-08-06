9551b6a1cefbfa916909393ac7951d39518f7c2d
wgebis <wgebis@gmail.com>
2018-08-06 12:14:15 +0200

    Add elk support

--- nisse/utils/logging.py
+++ nisse/utils/logging.py
@@ -12,7 +12,7 @@ def init_logging(application):
     if 'APP_CONFIG_FILE' in os.environ:
         env_name = os.environ['APP_CONFIG_FILE']
 
-    handler = CMRESHandler(hosts=[{'host': application.config["ELASTIC_HOST"], 'port': application.config.get["ELASTIC_PORT"]}],
+    handler = CMRESHandler(hosts=[{'host': application.config["ELASTIC_HOST"], 'port': application.config["ELASTIC_PORT"]}],
                        auth_type=CMRESHandler.AuthType.NO_AUTH,
                        es_index_name="logs-nisse",
                        es_additional_fields={'environment': env_name})
