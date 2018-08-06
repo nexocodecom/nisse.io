ea804e5b67faf151a9ca3a63b990de9891ad674b
wgebis <wgebis@gmail.com>
2018-08-06 12:26:31 +0200

    Add elk support

--- nisse/utils/logging.py
+++ nisse/utils/logging.py
@@ -4,19 +4,16 @@ from cmreslogging.handlers import CMRESHandler
 
 def init_logging(application):
 
-    application.logger.info('host: ' + application.config["ELASTIC_HOST"])
-    application.logger.info('port: ' + application.config["ELASTIC_PORT"])
+    if application.config['ELASTIC_HOST']:
+        env_name = 'development'
+        if 'APP_CONFIG_FILE' in os.environ:
+            env_name = os.environ['APP_CONFIG_FILE']
 
-    # if application.config['ELASTIC_HOST']:
-    env_name = 'development'
-    if 'APP_CONFIG_FILE' in os.environ:
-        env_name = os.environ['APP_CONFIG_FILE']
-
-    handler = CMRESHandler(hosts=[{'host': application.config["ELASTIC_HOST"], 'port': int(application.config["ELASTIC_PORT"])}],
-                       auth_type=CMRESHandler.AuthType.NO_AUTH,
-                       es_index_name="logs-nisse",
-                       es_additional_fields={'environment': env_name})
-    application.logger.addHandler(handler)
+        handler = CMRESHandler(hosts=[{'host': application.config["ELASTIC_HOST"], 'port': 9200}],
+                           auth_type=CMRESHandler.AuthType.NO_AUTH,
+                           es_index_name="logs-nisse",
+                           es_additional_fields={'environment': env_name})
+        application.logger.addHandler(handler)
 
     application.logger.setLevel(logging.DEBUG)
 
