d1593ce9d3d3acf3d1249dcf7129c018b392f3fa
wgebis <wgebis@gmail.com>
2019-01-25 13:31:08 +0100

    add new reminder message

--- nisse/services/reminder_job.py
+++ nisse/services/reminder_job.py
@@ -9,24 +9,88 @@ from nisse.utils.date_helper import *
 from nisse.utils.string_helper import get_full_class_name
 
 
-def get_users_to_notify(logger, config):
+def get_users_to_notify(logger, config, date_from: date, date_to: date):
     engine = create_engine(config['SQLALCHEMY_DATABASE_URI'])
     session_maker = sessionmaker(bind=engine)
     session = None
     try:
         session = session_maker()
         user_service = UserService(session, Bcrypt())
-        end_date = datetime.utcnow()
-        start_date = end_date - timedelta(minutes=5)
-        return user_service.get_users_to_notify(datetime.utcnow().date(), datetime.utcnow().weekday(),
-                                                start_date.time(),
-                                                end_date.time())
+
+        result = []
+        users = user_service.get_users_to_notify_last_period(minutes=5)
+        for user in users:
+            time_entries = user_service.get_time_entry_date_range(user.user_id, date_from, date_to)
+            days_reported = set(map(lambda te: te.report_date, time_entries))
+            all_days = set(date_range(date_from, date_to + timedelta(days=1)))
+
+            result.append([user, sorted(all_days - days_reported)])
+
+        return result
+
     except Exception as e:
         logger.error(e)
         raise
     finally:
         if session:
             session.close()
+
+
+def get_everyday_slack_message(config, remind_date):
+
+    return "It looks like you didn't report work time for `{0}`".format(remind_date.strftime("%A, %d %B")), [
+            {
+                "text": "Click 'Report' or use */ni* command:",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "callback_id": get_full_class_name(RemindTimeReportBtnPayload),
+                "actions": [
+                    {
+                        "name": "report",
+                        "text": "Report",
+                        "style": "success",
+                        "type": "button",
+                        'value': remind_date.strftime("%Y-%m-%d"),
+                    }
+                ],
+                "mrkdwn_in": ["text"]
+            },
+            {
+                "text": "",
+                "footer": config['MESSAGE_REMINDER_RUN_TIP'],
+                "mrkdwn_in": ["footer"]
+            },
+        ]
+
+
+def get_friday_slack_message(config, remind_dates):
+
+    actions = []
+    for (date) in remind_dates:
+        actions.append(
+            {
+                "name": "report",
+                "text": date.strftime("%A, %d %B"),
+                "style": "success",
+                "type": "button",
+                "value": date.strftime("%Y-%m-%d"),
+            })
+
+    return "It looks like you didn't report work time for `This week`", [
+            {
+                "text": "Click appropriate buttons or use */ni* command:",
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "callback_id": get_full_class_name(RemindTimeReportBtnPayload),
+                "actions": actions,
+                "mrkdwn_in": ["text"]
+            },
+            {
+                "text": "",
+                "footer": config['MESSAGE_REMINDER_RUN_TIP'],
+                "mrkdwn_in": ["footer"]
+            },
+        ]
 
 
 def remind(logger, config):
@@ -37,9 +101,14 @@ def remind(logger, config):
         return
 
     slack_client = SlackClient(config['SLACK_BOT_ACCESS_TOKEN'])
-    users = get_users_to_notify(logger, config)
+    is_friday = date.today().weekday() == 4
 
-    for user in users:
+    date_to = datetime.utcnow().date()
+    date_from = date_to - timedelta(days=4) if is_friday else date_to
+
+    users = get_users_to_notify(logger, config, date_from, date_to)
+
+    for (user, dates) in users:
         logger.info('Sending notification for user: ' + user.username)
 
         im_channel = slack_client.api_call(
@@ -50,34 +119,18 @@ def remind(logger, config):
             logger.error("Can't open im channel for: " +
                          str(user.user_id) + '. ' + im_channel["error"])
 
+        if is_friday and len(dates) > 1:
+            message = get_friday_slack_message(config, dates)
+        else:
+            message = get_everyday_slack_message(config, date_to)
+
+
         resp = slack_client.api_call(
             "chat.postMessage",
             channel=im_channel['channel']['id'],
-            text="It looks like you didn't report work time for `{0}`".format(remind_date.strftime("%A, %d %B")), # date formatted like Wednesday, 19
+            text=message[0],
+            attachments=message[1],
             mrkdwn=True,
-            attachments=[
-                {
-                    "text": "Click 'Report' or use */ni* command:",
-                    "color": "#3AA3E3",
-                    "attachment_type": "default",
-                    "callback_id": get_full_class_name(RemindTimeReportBtnPayload),                                       
-                    "actions": [
-                        {
-                            "name": "report",
-                            "text": "Report",
-                            "style": "success",
-                            "type": "button",
-                            'value': remind_date.strftime("%Y-%m-%d"), 
-                        }
-                    ],
-                    "mrkdwn_in": ["text"]
-                },
-                {
-                    "text": "",
-                    "footer": config['MESSAGE_REMINDER_RUN_TIP'],
-                    "mrkdwn_in": ["footer"]
-                },
-            ],
             as_user=True
         )
 
